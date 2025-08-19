a818c7f90f3b6127b8ad773ff3f116fcbf47f922
BreffJaun <braun_jeff@web.de>
2025-08-19 09:20:02 +0200

    Fixed the bug that the randomQUote was using several quotes in one consultation. So we fixed it with an State, default Quote and added a func to overwrite it directly onAppear.  Also addes some styling to the QUoteOfTheDayView. Everything in concertation of all group members.

--- Projektwoche1/Enums/Category.swift
+++ Projektwoche1/Enums/Category.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUICore
 
 
 enum Category: String, CaseIterable, Identifiable {
@@ -18,4 +19,35 @@ enum Category: String, CaseIterable, Identifiable {
     case miscellaneous = "Miscellaneous"
     
     var id: String { rawValue }
+    
+    var color: Color {
+        switch self {
+        case .movie: return .red
+        case .series: return .blue
+        case .literature: return .purple
+        case .music: return .orange
+        case .famousPeople: return .pink
+        case .games: return .green
+        case .miscellaneous: return .gray
+        }
+    }
+    
+    var gradient: LinearGradient {
+        switch self {
+        case .movie:
+            return LinearGradient(colors: [.red, .orange], startPoint: .topLeading, endPoint: .bottomTrailing)
+        case .series:
+            return LinearGradient(colors: [.blue, .cyan], startPoint: .topLeading, endPoint: .bottomTrailing)
+        case .literature:
+            return LinearGradient(colors: [.purple, .indigo], startPoint: .topLeading, endPoint: .bottomTrailing)
+        case .music:
+            return LinearGradient(colors: [.pink, .orange], startPoint: .topLeading, endPoint: .bottomTrailing)
+        case .famousPeople:
+            return LinearGradient(colors: [.mint, .teal], startPoint: .topLeading, endPoint: .bottomTrailing)
+        case .games:
+            return LinearGradient(colors: [.green, .yellow], startPoint: .topLeading, endPoint: .bottomTrailing)
+        case .miscellaneous:
+            return LinearGradient(colors: [.gray, .black], startPoint: .topLeading, endPoint: .bottomTrailing)
+        }
+    }
 }
